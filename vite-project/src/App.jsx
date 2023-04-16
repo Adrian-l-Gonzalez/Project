644ee1fbf782865f0c618a333b5378f0143f8e92
Adrian Gonzalez <adrianleogonza@gmail.com>
2023-04-16 10:42:07 -0300

    se creo el componente de la Nav

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import navBar from "./components/Navbar"
+
+function App() {
+  
+  return (
+   <>
+   <Navbar/>
+   </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+            <>
+           <h3>logo</h3>
+           <nav>
+            <a href="/#">Homme</a>
+            <a href="/#">Homme</a>
+            <a href="/#">Homme</a>
+            <a href="/#">Homme</a>           
+           </nav>
+            </>
+    )
+}
+
+export default Navbar;
