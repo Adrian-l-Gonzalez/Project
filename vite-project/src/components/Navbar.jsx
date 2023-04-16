95dbe7ce1a7ded6733623eb1031356d8cb0a2e15
Adrian Gonzalez <adrianleogonza@gmail.com>
2023-04-16 10:54:53 -0300

    se agrego iconos de reac-icons

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
 
 const Navbar = () => {
     return (
@@ -6,12 +7,17 @@ const Navbar = () => {
            <h3>logo</h3>
            <nav>
             <a href="/#">Homme</a>
-            <a href="/#">Homme</a>
-            <a href="/#">Homme</a>
-            <a href="/#">Homme</a>           
+            <a href="/#">My Work</a>
+            <a href="/#">Blog</a>
+            <a href="/#">About Me</a> 
+                <button>
+                <FaTimes/>
+                </button>    
+                <button>
+                    <FaBars/>
+                </button>      
            </nav>
             </>
     )
 }
-
 export default Navbar;
